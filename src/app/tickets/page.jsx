ecc40e2e6ade60385e9a502af6ff3dd2a1b9656e
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-06-19 21:04:02 +0600

    fixed file name

--- src/app/tickets/page.jsx
+++ src/app/tickets/page.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import AllTicketsContent from './AllTicketsContent.jsx';
+import AllTicketsContent from './AllTicketsContent';
 
 export default function AllTicketsPage() {
   return (
